a1cc3c6f75b69c30a184ea85e73a8a7168c4f8c0
Erdem Kardas <erdem.kardas@iem.thm.de>
2026-09-02 12:46:47 +0200

    time adjust

--- docs/script.js
+++ docs/script.js
@@ -189,7 +189,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
               currentIndex = nextIndex;
             }
           });
-        }, 6000);
+        }, 3000);
       })
       .catch(err => console.error("Fehler beim Laden der Bilder:", err));
   }
